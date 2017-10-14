c337f3663fc69dd4d18a29d45d1f95430806b98f
Christopher <32796106+EnCue@users.noreply.github.com>
2017-10-14 22:25:21 +0800

    Tester files

---
+++ Faustv1.1/Faustv1.1/Assets/Scripts/GitHubTestScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHubTestScript : MonoBehaviour {
+
+	void Awake(){
+		Debug.Log ("GitHub tester");
+	}
+}
